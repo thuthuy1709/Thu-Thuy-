24f1db04d77e1155eb16466860fbe7c0f1ed1475
thuthuy1709 <nguyenthuthuy7917@gmail.com>
2025-02-28 09:46:51 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function login()
+{
+  let u = username.value
+  let p = pass.value
+  if (u=="ThuThuy" && p=="170907")
+  {
+    window.open("home.html")
+    document.getElementById(compiler).style.display="block"
+  }
+}
